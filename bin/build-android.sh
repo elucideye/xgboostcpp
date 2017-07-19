420dff79a10dbc3c693f68acac3a240155fd116b
David Hirvonen <dhirvonen@elucideye.com>
2017-07-18 21:11:16 -0400

    size related flags and test results

--- bin/build-android.sh
+++ bin/build-android.sh
@@ -2,22 +2,31 @@
 
 CONFIG=MinSizeRel
 
+# see: src/test/CMakeLists.txt
+#   Add this flag in test-xgbooster to remove training code from app:
+#   target_compile_definitions(${test_app} PUBLIC XGBOOSTER_DO_LEAN=1)
+
+# Test 0 :
+# 720K	_install/android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections/bin/test-xgbooster
+#TOOLCHAIN=android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections
+#XGBOOSTER_DO_LEAN=OFF
+
 # Test 1 :
 # 640K	_install/android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections/bin/test-xgbooster
-# TOOLCHAIN=android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections
-# XGBOOSTER_DO_LEAN=ON
+#TOOLCHAIN=android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections
+#XGBOOSTER_DO_LEAN=ON
 
 # Test 2 :
-# 724K	_install/android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections-lto/bin/test-xgbooster
-TOOLCHAIN=android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections-lto
-XGBOOSTER_DO_LEAN=OFF
+# 708K	_install/android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections-lto/bin/test-xgbooster
+#TOOLCHAIN=android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections-lto
+#XGBOOSTER_DO_LEAN=OFF
 
 # Test 3 :
-# 640K	_install/android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections-lto/bin/test-xgbooster
-# TOOLCHAIN=android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections-lto
-# XGBOOSTER_DO_LEAN=ON
+# 632K	_install/android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections-lto/bin/test-xgbooster
+#TOOLCHAIN=android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections-lto
+#XGBOOSTER_DO_LEAN=ON
 
-INSTALL=--strip
+INSTALL="--strip"
 
 ARGS=(
     --verbose
