0fb4d2744cfb221e9c2e9f24300d639e5b8bed8a
David Hirvonen <dhirvonen@elucideye.com>
2017-07-18 20:16:24 -0400

    add multiple configuration sets for android build

--- bin/build-android.sh
+++ bin/build-android.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 CONFIG=MinSizeRel
+
+# Test 1 :
+# 640K	_install/android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections/bin/test-xgbooster
+# TOOLCHAIN=android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections
+# XGBOOSTER_DO_LEAN=ON
+
+# Test 2 :
+# 724K	_install/android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections-lto/bin/test-xgbooster
 TOOLCHAIN=android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections-lto
+XGBOOSTER_DO_LEAN=OFF
+
+# Test 3 :
+# 640K	_install/android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections-lto/bin/test-xgbooster
+# TOOLCHAIN=android-ndk-r10e-api-19-armeabi-v7a-neon-hid-sections-lto
+# XGBOOSTER_DO_LEAN=ON
+
 INSTALL=--strip
 
 ARGS=(
@@ -13,6 +28,7 @@ ARGS=(
     XGBOOSTER_SERIALIZE_WITH_BOOST=OFF
     XGBOOSTER_SERIALIZE_WITH_CEREAL=ON
     XGBOOSTER_ADD_TO_STRING=ON
+    XGBOOSTER_DO_LEAN=${XGBOOSTER_DO_LEAN}
 )
 
-polly.py --toolchain ${TOOLCHAIN} ${ARGS[@]} ${INSTALL} --reconfig # --test
+polly.py --toolchain ${TOOLCHAIN} ${ARGS[@]} ${INSTALL} --clear
